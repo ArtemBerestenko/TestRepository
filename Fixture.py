762fd51502c427376e195532d3eb9014c599daf2
Artem Berestenko <aberes@DP794.softservecom.com>
2015-12-09 15:45:45 +0200

    added example file with using fixtures for nose and unittest

---
+++ Fixture.py
@@ -0,0 +1,54 @@
+__author__ = 'aberes'
+
+
+# This module is cteted to show differance between fixtures in Unittest and Nose
+# You must run it with both nose and unittest:
+# Do not forget to use param --nocapture for nose:
+
+
+def setUpModule():
+    print "In Setup Module"
+
+
+def tearDownModule():
+    print "In tear down Module"
+
+
+# @unittest.skip("weve")
+# class TestOfFixture_Unit_Test(unittest.TestCase):
+#     def setUp(self):
+#         print "in Setup test"
+#
+#     def tearDown(self):
+#         print "In Tear Down"
+#
+#     @classmethod
+#     def setUpClass(cls):
+#         print "in Setup Class"
+#
+#     @classmethod
+#     def tearDownClass(cls):
+#         print "In Tear Down Class"
+#
+#     def testmethod(self):
+#         self.assertTrue(1==1)
+
+
+def setup_func():
+    print "in setup test"
+
+
+def teardown_func():
+    print "in tear down"
+
+
+class TestOfFixture_Nose():
+    @classmethod
+    def setUpClass(self):
+        print "in Setup Class"
+
+    def setUp(self):
+        print "in Setup test"
+
+    def test(self):
+        print "some test code"
